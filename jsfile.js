ee39cfd3602bb65d34549af8b6127e4581a66070
Innegativeion <itravis888@gmail.com>
2023-09-05 13:17:01 -0400

    Updated Some character designs and animations

--- jsfile.js
+++ jsfile.js
@@ -2,7 +2,7 @@ var imageThumbs = document.getElementById("image-thumbs");
 
 var currentImage = document.getElementById("current-image");
 
-for (var i = 1; i <= 7; i++) {
+for (var i = 1; i <= 8; i++) {
 var thumb = document.createElement("img");
 thumb.src = "pictures/InUseCharacter/Image" + i + ".png";
 thumb.alt = "Image " + i;
